b56d659852748066a6996f9c537421177b6ed6de
AndreyMilkevich <chemko@list.ru>
2022-03-21 10:48:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+}
+class Test1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int a = s.nextInt();
+        System.out.println("Введите второе число");
+        int b = s.nextInt();
+        int c = a+b;
+        System.out.println("Ответ: " + c);
+        }
+    }
